kevinbruyne0125/image_optim

commit 318d698a825222ecf9b77de0688f5cf574c8b9bf

move conversion to ImagePaths from run_method_for

lib/image_optim.rb
@@ -114,14 +114,14 @@ class ImageOptim
   # if block given yields path and result for each image and returns array of yield results
   # else return array of results
   def optimize_images(paths, &block)
-    run_method_for(paths, :optimize_image, &block)
+    run_method_for(paths.map{ |path| ImagePath.new(path) }, :optimize_image, &block)
   end
 
   # Optimize multiple images in place
   # if block given yields path and result for each image and returns array of yield results
   # else return array of results
   def optimize_images!(paths, &block)
-    run_method_for(paths, :optimize_image!, &block)
+    run_method_for(paths.map{ |path| ImagePath.new(path) }, :optimize_image!, &block)
   end
 
   # Optimization methods with default options
@@ -163,7 +163,6 @@ private
   # Run method for each path and yield each path and result if block given
   def run_method_for(paths, method_name, &block)
     apply_threading(paths).map do |path|
-      path = ImagePath.new(path)
       result = send(method_name, path)
       if block
         block.call(path, result)

commit 65122e6cca258119b52a8ffb10b44f8ba2ec69b8

there is not need to convert paths to ImagePath for run_method_for, removes regression introduced in 318d698a82

lib/image_optim.rb
@@ -108,14 +108,14 @@ class ImageOptim
   # if block given yields path and result for each image and returns array of yield results
   # else return array of results
   def optimize_images(paths, &block)
-    run_method_for(paths.map{ |path| ImagePath.convert(path) }, :optimize_image, &block)
+    run_method_for(paths, :optimize_image, &block)
   end
 
   # Optimize multiple images in place
   # if block given yields path and result for each image and returns array of yield results
   # else return array of results
   def optimize_images!(paths, &block)
-    run_method_for(paths.map{ |path| ImagePath.convert(path) }, :optimize_image!, &block)
+    run_method_for(paths, :optimize_image!, &block)
   end
 
   # Optimize multiple image datas

spec/image_optim_spec.rb
@@ -170,12 +170,6 @@ describe ImageOptim do
   describe "optimize multiple" do
     let(:srcs){ ('a'..'z').to_a }
 
-    before do
-      srcs.each do |src|
-        ImageOptim::ImagePath.should_receive(:new).with(src).and_return(src)
-      end
-    end
-
     %w[optimize_images optimize_images!].each do |list_method|
       describe list_method do
         single_method = list_method.sub('images', 'image')